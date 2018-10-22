3fd5d71af554a98c8b2c3ac081b6958a652086a4
zero <grayautumn15@126.com>
2018-10-22 14:03:42 +0800

    update

--- scripts/tiff_exists_check.py
+++ scripts/tiff_exists_check.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 import sys
 import openslide
 
@@ -8,9 +9,13 @@ from common.tslide.tslide import TSlide
 
 TIFF_FILES_PATH = '/home/cnn/Development/DATA/TRAIN_DATA/TIFFS'
 
+REMOTE_TIFF_PATH = "/run/user/1000/gvfs/smb-share:server=192.168.2.221,share=data_samba/DATA/0TIFF/before_20181016"
+
 
 def main(file_path):
     tiff_dict = generate_name_path_dict(TIFF_FILES_PATH, ['.kfb'])
+    remote_tiff_dict = generate_name_path_dict(REMOTE_TIFF_PATH, ['.kfb'])
+
     with open(file_path) as f:
         lines = f.readlines()
         items = [line.replace('\n', '') for line in lines]
@@ -19,17 +24,19 @@ def main(file_path):
     for item in items:
         if item not in tiff_dict:
             miss_tiff_lst.append(item)
-            continue
+            remote_file_path = remote_tiff_dict[item]
+            print("COPY FILE ...\nFROM %s\nTO %s" % (remote_file_path, TIFF_FILES_PATH))
+            shutil.copy(remote_file_path, TIFF_FILES_PATH)
             # raise Exception('TIFF %s IS NOT FOUND IN LOCAL RESOURCE' % item)
 
-        path = tiff_dict[item]
-        try:
-            try:
-                slide = openslide.OpenSlide(path)
-            except:
-                slide = TSlide(path)
-        except Exception as e:
-            raise Exception("%s %s" % (item, str(e)))
+        # path = tiff_dict[item]
+        # try:
+        #     try:
+        #         slide = openslide.OpenSlide(path)
+        #     except:
+        #         slide = TSlide(path)
+        # except Exception as e:
+        #     raise Exception("%s %s" % (item, str(e)))
 
     print('\n'.join(miss_tiff_lst))
 
