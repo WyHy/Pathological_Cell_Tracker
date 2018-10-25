de7d2ddc4b1b865fcd5225657b1a7e69e142cba0
zero <grayautumn15@126.com>
2018-10-25 16:11:33 +0800

    update

--- scripts/tiff_exists_check.py
+++ scripts/tiff_exists_check.py
@@ -4,15 +4,45 @@ import sys
 import openslide
 
 sys.path.append('..')
-from utils import generate_name_path_dict
 from common.tslide.tslide import TSlide
+from utils import generate_name_path_dict, FilesScanner
 
 TIFF_FILES_PATH = '/home/cnn/Development/DATA/TRAIN_DATA/TIFFS/20181025'
 
 REMOTE_TIFF_PATH = "/run/user/1000/gvfs/smb-share:server=192.168.2.221,share=data_samba/DATA/0TIFF"
 
 
-def main(file_path):
+def tiff_readable_check(path):
+    """
+    病理图像可读性验证
+    :param path: 原图路径
+    :return:
+    """
+
+    files = FilesScanner(path, ['.tif', 'kfb']).get_files()
+    filename_lst = []
+    filepath_lst = []
+
+    for file in files:
+        basename = os.path.basename(file)
+
+        if basename in filename_lst:
+            raise Exception("%s\n%s" % (file, filepath_lst[filename_lst.index(basename)]))
+        else:
+            filename_lst.append(basename)
+            filepath_lst.append(file)
+
+    for file in files:
+        try:
+            try:
+                slide = openslide.OpenSlide(file)
+            except:
+                slide = TSlide(file)
+        except Exception as e:
+            raise Exception("%s %s" % (file, str(e)))
+
+
+def get_and_download(file_path):
     tiff_dict = generate_name_path_dict(TIFF_FILES_PATH, ['.kfb'])
     remote_tiff_dict = generate_name_path_dict(REMOTE_TIFF_PATH, ['.kfb'])
 
@@ -45,4 +75,6 @@ def main(file_path):
 
 
 if __name__ == '__main__':
-    main('work_tiff_list_01.txt')
+    # 检查文件名是否有重复
+    tiff_readable_check(REMOTE_TIFF_PATH)
+    # get_and_download('work_tiff_list_01.txt')
