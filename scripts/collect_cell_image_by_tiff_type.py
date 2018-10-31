40656e4c076ac8987053b3e68f308a37d32e8004
zero <grayautumn15@126.com>
2018-10-31 12:32:28 +0800

    update

--- scripts/collect_cell_image_by_tiff_type.py
+++ scripts/collect_cell_image_by_tiff_type.py
@@ -23,7 +23,7 @@ def generate_task_lst(path, name):
     nrows = sheet.nrows
 
     with open(name, 'w') as o:
-        for i in range(1, nrows):
+        for i in range(0, nrows):
             tiff_name, tiff_type = sheet.row_values(i)
             if tiff_name:
                 o.write("%s\n" % tiff_name)
@@ -45,7 +45,7 @@ def get_tiff_name_and_type(path):
     # 获取行总数
     nrows = sheet.nrows
 
-    for i in range(1, nrows):
+    for i in range(0, nrows):
         tiff_name, tiff_type = sheet.row_values(i)
         dict_[tiff_name.replace(" ", "-")] = tiff_type.upper()
 
