aaa74ff7fbed72a7e225dd4dee19b5ba095cc9ad
zero <grayautumn15@126.com>
2018-11-02 17:49:02 +0800

    update

---
+++ scripts/select_useful_tiff_from_excel.py
@@ -0,0 +1,31 @@
+import json
+
+
+def load_dict(file_path):
+    with open(file_path) as f:
+        lines = f.readlines()
+
+        dict_ = {}
+        for line in lines:
+            key, value = line.replace('\n', '').split('\t')
+            value = json.loads(value)
+            dict_[key.replace(' ', '-')] = value['zhu'] if value['zhu'] != 42 else value['doctor']
+
+        return dict_
+
+
+if __name__ == '__main__':
+    dict_ = load_dict('DIAGNOSE_RESULT_DICT.txt')
+    CLASSES = ['AGC', 'VIRUS', 'EC', 'FUNGI', 'ACTINO']
+    COUNT = [0] * len(CLASSES)
+
+    for key, value in dict_.items():
+        if value in CLASSES:
+            COUNT[COUNT.index(value)] += 1
+
+    print(CLASSES)
+    print(COUNT)
+
+
+
+
