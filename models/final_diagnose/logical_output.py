f318b38350227718a9573aa013fb83e196d66fbc
zero <grayautumn15@126.com>
2018-11-13 21:00:51 +0800

    update

--- models/final_diagnose/logical_output.py
+++ models/final_diagnose/logical_output.py
@@ -67,7 +67,8 @@ def read_clas_xmls(csv_path):
         next(lines, None)
         for line in lines:
             name, label01, accu01, label02, accu02, xmin, ymin, w, h = line
-            accu01, accu02, xmin, ymin, w, h = float(accu01), float(accu02), float(xmin), float(ymin), float(w), float(h)
+            accu01, accu02, xmin, ymin, w, h = float(accu01), float(accu02), float(xmin), float(ymin), float(w), float(
+                h)
             x0, y0 = [int(item) for item in name.split('_')]
 
             x, y = xmin + x0, ymin + y0
@@ -89,29 +90,53 @@ def read_clas_xmls(csv_path):
     return points_lst
 
 
-def squa_decision(clas_18, cell_count_dict):
+def squa_decision(clas_18, cell_count_dict, thr=5):
     """
 
     :param clas_18:
     :param cell_count_dict:
     :return: label
     """
-    impact_category = []
+    impact_category_dict = {}
 
     for item in SQUA_CELL_CATEGORY:
         cell_count = len(clas_18[item])
-        if cell_count == 0:
+        if cell_count < thr:
             continue
 
-        # if cell_count > cell_count_dict[item]:
-        impact_category.append((item, cell_count))
+        impact_category_dict[item] = cell_count
 
-    impact_category = sorted(impact_category, key=lambda category: category[1], reverse=True)
+    if not impact_category_dict:
+        return None
+    print(impact_category_dict)
 
-    return [category for category in impact_category[:3]]
+    impact_category_lst = sorted(impact_category_dict.items(), key=lambda category: category[1], reverse=True)
+    if impact_category_lst[0][0] == 'LSIL_E':
+        return 'LSIL', impact_category_lst
+
+    if impact_category_lst[0][0] == 'LSIL_F':
+        if 'LSIL_E' in impact_category_dict and impact_category_dict['LSIL_E'] > 2 * thr:
+            return 'LSIL', impact_category_lst
+        return 'ASCUS', impact_category_lst
+
+    if impact_category_lst[0][0] == 'HSIL_S':
+        return 'HSIL', impact_category_lst
+
+    if impact_category_lst[0][0] == 'HSIL_M' or impact_category_lst[0][0] == 'HSIL_B':
+        if 'HSIL_S' in impact_category_dict:
+            if impact_category_dict['HSIL_S'] > 2 * thr:
+                return 'HSIL', impact_category_lst
+            else:
+                return "ASCH", impact_category_lst
+        return None
+
+    if impact_category_lst[0][0] in ['SCC_G', 'SCC_R']:
+        return "SCC", impact_category_lst
+
+    return None
 
 
-def infect_decision(clas_18, cell_count_dict):
+def infect_decision(clas_18, cell_count_dict, thr=150):
     """
 
     :param clas_18:
@@ -119,7 +144,7 @@ def infect_decision(clas_18, cell_count_dict):
     :return: label
     """
     category_max_label = None
-    category_max_count = 0
+    category_max_count = thr
 
     for item in INFECT_CELL_CATEGORY:
         cell_count = len(clas_18[item])
@@ -131,7 +156,7 @@ def infect_decision(clas_18, cell_count_dict):
     return (category_max_label, category_max_count) if category_max_label else None
 
 
-def gland_decision(clas_18, cell_count_dict):
+def gland_decision(clas_18, cell_count_dict, thr=20):
     """
 
     :param clas_18:
@@ -139,7 +164,7 @@ def gland_decision(clas_18, cell_count_dict):
     :return: label
     """
     category_max_label = None
-    category_max_count = 0
+    category_max_count = thr
 
     for item in GLAND_CELL_CATEGORY:
         cell_count = len(clas_18[item])
@@ -161,7 +186,13 @@ def diagnose_from_categories_18(clas_18):
     res_infect = infect_decision(clas_18, IMPACT_CATEGORY_CELL_COUNT_DICT)
     res_gland = gland_decision(clas_18, IMPACT_CATEGORY_CELL_COUNT_DICT)
 
-    return [res_squa, res_infect, res_gland]
+    print(res_squa)
+    print(res_infect)
+    print(res_gland)
+
+    result = "+".join([item[0] for item in [res_squa, res_infect, res_gland] if item])
+
+    return result if result else "NORMAL"
 
 
 def gen_categories_normal_4(points_lst):
@@ -175,7 +206,8 @@ def gen_categories_normal_4(points_lst):
     for item in NORMAL_CATEGORY:
         normal_dict[item] = []
 
-    [normal_dict[point['xception']['label']].append(point) for point in points_lst if point['xception']['label'] in normal_dict]
+    [normal_dict[point['xception']['label']].append(point) for point in points_lst if
+     point['xception']['label'] in normal_dict]
 
     return normal_dict
 
@@ -192,7 +224,8 @@ def gen_categories_abnormal_14(points_lst, threshes_dict):
         abnormal_dict[item] = []
 
     for point in points_lst:
-        label_yolo, acc_yolo, label_xcp, acc_xcp = point['yolo']['label'], point['yolo']['accuracy'], point['xception']['label'], point['xception']['accuracy']
+        label_yolo, acc_yolo, label_xcp, acc_xcp = point['yolo']['label'], point['yolo']['accuracy'], point['xception'][
+            'label'], point['xception']['accuracy']
         if label_yolo != label_xcp:
             if label_xcp in NORMAL_CATEGORY:
                 continue
@@ -237,8 +270,7 @@ def slide_diagnose(points_lst, threshes_dict):
 
 
 def gen_slide_diagnose(csv_path):
-    result = slide_diagnose(read_clas_xmls(csv_path), IMPACT_CATEGORY_THRESH_DICT)
-    return result
+    return slide_diagnose(read_clas_xmls(csv_path), IMPACT_CATEGORY_THRESH_DICT)
 
 
 if __name__ == '__main__':
