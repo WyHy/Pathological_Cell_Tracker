b951bd9061e4cd471d44dad3daeb3f9876886d74
zero <grayautumn15@126.com>
2018-11-16 15:59:36 +0800

    update

---
+++ scripts/collect_by_tiff_type.py
@@ -0,0 +1,40 @@
+import os
+import shutil
+
+
+def load_dict(file_path):
+    with open(file_path) as f:
+        lines = f.readlines()
+
+        dict_ = {}
+        for line in lines:
+            key, value = line.replace('\n', '').split('\t')
+            key = key.replace(' ', '-')
+            dict_[key] = value
+
+        return dict_
+
+
+def do_collect_by_tiff_type(path, dict_):
+    """
+    根据类别及分类别数量限制收集分类图像
+    :param path: CELLS 文件路径
+    :param dict_:  TIFF 文件名及对应 诊断结果 dict
+    :return:
+    """
+    files = os.listdir(path)
+    total = len(files)
+    for index, tiff in enumerate(files):
+        print("%s / %s ..." % (index + 1, total))
+        src_root_dir = os.path.join(path, tiff)
+        types = os.listdir(src_root_dir)
+        for item in types:
+            src_path = os.path.join(src_root_dir, item)
+            dst_path = os.path.join(os.path.dirname(path), 'TO_BE_CHECK_CELLS', dict_[tiff], tiff, item)
+            shutil.copytree(src_path, dst_path)
+
+
+if __name__ == '__main__':
+    cell_path = '/home/cnn/Development/DATA/CELL_CLASSIFICATION_20181114_BATCH_4_FOR_FINAL_DIAGNOSE/CELLS'
+    dict_path = 'name_and_labels.txt'
+    do_collect_by_tiff_type(cell_path, load_dict(dict_path))
